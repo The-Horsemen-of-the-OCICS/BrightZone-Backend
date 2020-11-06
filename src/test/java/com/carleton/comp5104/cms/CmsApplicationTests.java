333231b0cc53b1816be3d6770ebcc3556c131b6a
YuSun <yusun@example.com>
2020-11-06 15:44:51 -0500

    initial

---
+++ src/test/java/com/carleton/comp5104/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carleton.comp5104.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
